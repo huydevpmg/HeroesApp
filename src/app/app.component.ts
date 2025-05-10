e04ab60e3fe5ba069e805b4b338f42ffec39674c
Huy Phan <huy.pmg16@gmail.com>
2025-05-10 12:08:21 +0700

    fix: rebuild UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HeroesApp';
+  showNavbar: boolean = true;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.url.subscribe(url => {
+      this.showNavbar = !url.some(segment => segment.path === 'login');
+    });
+  }
 }
